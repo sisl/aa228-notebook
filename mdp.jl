59b047c0a98784dea0791565631bdbf2418c86cb
Mykel Kochenderfer <mykel@stanford.edu>
2014-08-29 19:07:09 -0700

    Added value iteration

--- mdp.jl
+++ mdp.jl
@@ -7,9 +7,6 @@ type GridWorld
   A
   T
   R
-  gamma
-  V
-  Q
 end
 
 s2xy(s) = ind2sub((10, 10), s)
@@ -45,28 +42,28 @@ function GridWorld()
           R[s, :] = -0.1
         end
 
-        R[s, 3] = -1
+        R[s, 3] = -0.7
       elseif x == 10
         if y == 1 || y == 10
           R[s, :] = -0.2
         else
           R[s, :] = -0.1
         end
-        R[s, 4] = -1
+        R[s, 4] = -0.7
       elseif y == 1
         if x == 1 || x == 10
           R[s, :] = -0.2
         else
           R[s, :] = -0.1
         end
-        R[s, 1] = -1
+        R[s, 1] = -0.7
       elseif y == 10
         if x == 1 || x == 10
           R[s, :] = -0.2
         else
           R[s, :] = -0.1
         end
-        R[s, 2] = -1
+        R[s, 2] = -0.7
       end
       for a in A
         if a == 1 # left
@@ -93,13 +90,15 @@ function GridWorld()
       end
     end
   end
-  gamma = 0.9
-  V = zeros(100)
-  Q = zeros(100,4)
-  GridWorld(S,A,T,R,gamma,V,Q)
+  R[1,1] = -0.8
+  R[10,1] = -0.8
+  R[91,2] = -0.8
+  R[100,2] = -0.8
+  GridWorld(S,A,T,R)
 end
 
 function colorval(val, brightness::Real = 1.0)
+  val = float(val)
   x = 255 - min(255, 255 * (abs(val) ./ 10.0) .^ brightness)
   r = 255 * ones(size(val))
   g = 255 * ones(size(val))
@@ -111,9 +110,30 @@ function colorval(val, brightness::Real = 1.0)
   (r, g, b)
 end
 
-function plot(obj::GridWorld)
-  V = obj.V
-  Q = obj.Q
+function plot(obj::GridWorld, f::Function)
+  V = map(f, obj.S)
+  plot(obj, V)
+end
+
+function plot(obj::GridWorld, V::Vector)
+  o = IOBuffer()
+  sqsize = 1.0
+  twid = 0.05
+  (r, g, b) = colorval(V)
+  for s = obj.S
+    (yval, xval) = s2xy(s)
+    yval = 10 - yval
+    println(o, "\\definecolor{currentcolor}{RGB}{$(r[s]),$(g[s]),$(b[s])}")
+    println(o, "\\fill[currentcolor] ($((xval-1) * sqsize),$((yval) * sqsize)) rectangle +($sqsize,$sqsize);")
+    vs = @sprintf("%0.2f", V[s])
+    println(o, "\\node[above right] at ($((xval-1) * sqsize), $((yval) * sqsize)) {\$$(vs)\$};")
+  end
+  println(o, "\\draw[black] grid(10,10);")
+  tikzDeleteIntermediate(false)
+  TikzPicture(takebuf_string(o), options="scale=1.25")
+end
+
+function plot(obj::GridWorld, V::Vector, policy::Vector)
   o = IOBuffer()
   sqsize = 1.0
   twid = 0.05
@@ -131,19 +151,19 @@ function plot(obj::GridWorld)
     c = [xval, yval] * sqsize - sqsize / 2
     C = [c'; c'; c']'
     RightArrow = [0 0 sqsize/2; twid -twid 0]
-    if V[s] == Q[s,1] # left
+    if policy[s] == 1 # left
       A = [-1 0; 0 -1] * RightArrow + C
       println(o, "\\fill ($(A[1]), $(A[2])) -- ($(A[3]), $(A[4])) -- ($(A[5]), $(A[6])) -- cycle;")
     end
-    if V[s] == Q[s,2] # right
+    if policy[s] == 2 # right
       A = RightArrow + C
       println(o, "\\fill ($(A[1]), $(A[2])) -- ($(A[3]), $(A[4])) -- ($(A[5]), $(A[6])) -- cycle;")
     end
-    if V[s] == Q[s,3] # up
+    if policy[s] == 3 # up
       A = [0 -1; 1 0] * RightArrow + C
       println(o, "\\fill ($(A[1]), $(A[2])) -- ($(A[3]), $(A[4])) -- ($(A[5]), $(A[6])) -- cycle;")
     end
-    if V[s] == Q[s,4] # down
+    if policy[s] == 4 # down
       A = [0 1; -1 0] * RightArrow + C
       println(o, "\\fill ($(A[1]), $(A[2])) -- ($(A[3]), $(A[4])) -- ($(A[5]), $(A[6])) -- cycle;")
     end
@@ -153,7 +173,6 @@ function plot(obj::GridWorld)
   end
   println(o, "\\end{scope}");
   println(o, "\\draw[black] grid(10,10);");
-  tikzDeleteIntermediate(false)
   TikzPicture(takebuf_string(o), options="scale=1.25")
 end
 
@@ -171,4 +190,23 @@ function backupGaussSeidel(obj::GridWorld)
     obj.V[s] = maximum(obj.Q[s,:])
   end
 end
+
+g = GridWorld()
+
+actionMap = [:left=>1, :right=>2, :up=>3, :down=>4]
+function T(s0, a::Integer, s1)
+  g.T[s0, a, s1]
+end
+function T(s0, a::Symbol, s1)
+  g.T[s0, actionMap[a], s1]
+end
+function R(s, a::Integer)
+  g.R[s, a]
+end
+function R(s, a::Symbol)
+  g.R[s, actionMap[a]]
+end
+S = g.S
+A = [:left, :right, :up, :down]
+discount = 0.9
 
