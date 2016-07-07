2429a5783d6a6c205b671f6167a920a86a95c727
smostars <smostarshed@gmail.com>
2016-07-07 13:21:23 -0700

    Add files via upload
    Tim fixed the asynchronous code problem, by implementing tuple signaling correctly. Implemented new functions (changed the name of old functions to name_old), plus a new string formatting function.

--- bandits.jl
+++ bandits.jl
@@ -5,23 +5,65 @@ Bandit(k::Integer) = Bandit(rand(k))
 pull(b::Bandit, i::Integer) = rand() < b.θ[i]
 numArms(b::Bandit) = length(b.θ)
 
-function banditTrial(b)
+# New string formatting function
+function _get_latex_string_list_of_percentages{R<:Real}(bandit_odds::Vector{R})
+    strings = map(θ->@sprintf("%.2f percent", 100θ), bandit_odds)
+    retval = strings[1]
+    for i in 2 : length(strings)
+        retval = retval * ", " * strings[i]
+    end
+    latex(retval)
+end
+
+function banditTrial(b)       #new version implements the number of wins & tries as one signal, rather than two 
+    B = [button("Arm $i") for i = 1:numArms(b)]
+    bandit_states = [foldp((acc, value)->(acc[1]+pull(b,i),acc[2]+1), (0,0), signal(B[i])) for i in 1:arms]
+    for i = 1:numArms(b)
+        display(B[i])
+        display(map(s -> latex(@sprintf("%d wins out of %d tries (%d percent)", s[1], s[2], 100*s[1]/s[2])), bandit_states[i]))
+    end
+    t = togglebuttons(["Hide", "Show"], value="Hide", label="True parameters")
+    display(t)
+    display(map(v -> v == "Show" ? _get_latex_string_list_of_percentages(b.θ) : latex(""), signal(t)))
+end
+
+function banditTrial_old(b)
   B = [button("Arm $i") for i = 1:numArms(b)]
-  tries = [foldp((acc, value) -> acc + 1, 0, signal(B[i])) for i = 1:arms]
   wins = [foldp((acc, value) -> acc + pull(b,i), 0, signal(B[i])) for i = 1:arms]
+  tries = [foldp((acc, value) -> acc + 1, 0, signal(B[i])) for i = 1:arms]
   for i = 1:numArms(b)
     display(B[i])
     display(map((w,t) -> latex(@sprintf("%d wins out of %d tries (%d percent)", w, t, 100*w/t)), wins[i], tries[i]))
   end
   t = togglebuttons(["Hide", "Show"], value="Hide", label="True parameters")
   display(t)
-  display(map(v -> v == "Show" ? latex(string(b.θ)) : latex(""), signal(t)))
+  display(map(v -> v == "Show" ? latex(string(b.θ)) : latex(""), t))
 end
 
-function banditEstimation(b)
+function banditEstimation(b)     #new version implements the number of wins & tries as one signal, rather than two
   B = [button("Arm $i") for i = 1:numArms(b)]
-  tries = [foldp((acc, value) -> acc + 1, 0, signal(B[i])) for i = 1:arms]
+  bandit_states = [foldp((acc, value)->(acc[1]+pull(b,i),acc[2]+1), (0,0), signal(B[i])) for i in 1:arms]
+  for i = 1:numArms(b)
+      display(B[i])
+      display(map(s -> latex(@sprintf("%d wins out of %d tries (%d percent)", s[1], s[2], 100*s[1]/s[2])), bandit_states[i]))
+  end
+  display(map((w1,t1,w2,t2)->
+       Axis([
+              Plots.Linear(θ->pdf(Beta(w1+1, t1-w1+1), θ), (0,1), legendentry="Beta($(w1+1), $(t1-w1+1))"),
+              Plots.Linear(θ->pdf(Beta(w2+1, t2-w2+1), θ), (0,1), legendentry="Beta($(w2+1), $(t2-w2+1))")
+              ],
+            xmin=0,xmax=1,ymin=0),
+       wins[1], tries[1], wins[2], tries[2]
+       ))
+  t = togglebuttons(["Hide", "Show"], value="Hide", label="True parameters")
+  display(t)
+  display(map(v -> v == "Show" ? _get_latex_string_list_of_percentages(b.θ) : latex(""), signal(t)))
+end
+
+function banditEstimation_old(b)
+  B = [button("Arm $i") for i = 1:numArms(b)]
   wins = [foldp((acc, value) -> acc + pull(b,i), 0, signal(B[i])) for i = 1:arms]
+  tries = [foldp((acc, value) -> acc + 1, 0, signal(B[i])) for i = 1:arms]
   for i = 1:numArms(b)
     display(B[i])
     display(map((w,t) -> latex(@sprintf("%d wins out of %d tries (%d percent)", w, t, 100*w/t)), wins[i], tries[i]))
@@ -36,7 +78,7 @@ function banditEstimation(b)
        ))
   t = togglebuttons(["Hide", "Show"], value="Hide", label="True parameters")
   display(t)
-  display(map(v -> v == "Show" ? latex(string(b.θ)) : latex(""), signal(t)))
+  display(map(v -> v == "Show" ? latex(string(b.θ)) : latex(""), t))
 end
 
 type BanditStatistics
