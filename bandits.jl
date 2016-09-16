efee75d13e64ef02533aaf1d3d470164b52850b6
Tim Wheeler <wheelert@stanford.edu>
2016-09-15 20:32:14 -0700

    updated bandits.jl and lecture 10 so that it would not error out due to changes in Interact or Reactive. This closes #1

--- bandits.jl
+++ bandits.jl
@@ -1,85 +1,58 @@
 type Bandit
-  θ::Vector{Float64}
+  θ::Vector{Float64} # true bandit probabilities
 end
 Bandit(k::Integer) = Bandit(rand(k))
 pull(b::Bandit, i::Integer) = rand() < b.θ[i]
 numArms(b::Bandit) = length(b.θ)
 
-# New string formatting function
-function _get_latex_string_list_of_percentages{R<:Real}(bandit_odds::Vector{R})
+function _get_string_list_of_percentages{R<:Real}(bandit_odds::Vector{R})
     strings = map(θ->@sprintf("%.2f percent", 100θ), bandit_odds)
     retval = strings[1]
     for i in 2 : length(strings)
         retval = retval * ", " * strings[i]
     end
-    latex(retval)
+    # latex(retval)
+    retval
 end
 
-function banditTrial(b)       #new version implements the number of wins & tries as one signal, rather than two 
-    B = [button("Arm $i") for i = 1:numArms(b)]
-    bandit_states = [foldp((acc, value)->(acc[1]+pull(b,i),acc[2]+1), (0,0), signal(B[i])) for i in 1:arms]
-    for i = 1:numArms(b)
-        display(B[i])
-        display(map(s -> latex(@sprintf("%d wins out of %d tries (%d percent)", s[1], s[2], 100*s[1]/s[2])), bandit_states[i]))
+function banditTrial(b)
+
+    for i in 1 : numArms(b)
+        but = button("Arm $i")
+        display(but)
+        sig = foldp((acc, value)->(acc[1]+pull(b,i),acc[2]+1), (0,0), signal(but))
+        display(map(s -> @sprintf("%d wins out of %d tries (%d percent)", s[1], s[2], 100*s[1]/s[2]), sig))
+        # NOTE: we used to use the latex() wrapper
     end
-    t = togglebuttons(["Hide", "Show"], value="Hide", label="True parameters")
+
+    t = togglebuttons(["Hide", "Show"], value="Hide", label="True Params")
     display(t)
-    display(map(v -> v == "Show" ? _get_latex_string_list_of_percentages(b.θ) : latex(""), signal(t)))
+    display(map(v -> v == "Show" ? _get_string_list_of_percentages(b.θ) : "", signal(t)))
 end
 
-function banditTrial_old(b)
+function banditEstimation(b)
   B = [button("Arm $i") for i = 1:numArms(b)]
-  wins = [foldp((acc, value) -> acc + pull(b,i), 0, signal(B[i])) for i = 1:arms]
-  tries = [foldp((acc, value) -> acc + 1, 0, signal(B[i])) for i = 1:arms]
-  for i = 1:numArms(b)
-    display(B[i])
-    display(map((w,t) -> latex(@sprintf("%d wins out of %d tries (%d percent)", w, t, 100*w/t)), wins[i], tries[i]))
+  for (i,but) in enumerate(B)
+      display(but)
   end
-  t = togglebuttons(["Hide", "Show"], value="Hide", label="True parameters")
-  display(t)
-  display(map(v -> v == "Show" ? latex(string(b.θ)) : latex(""), t))
-end
+  sigs = [foldp((acc, value)->(acc[1]+pull(b,i),acc[2]+1), (0,0), signal(B[i])) for i in 1:numArms(b)]
+  for (i,sig) in enumerate(sigs)
+      display(map(s -> @sprintf("%d wins out of %d tries (%d percent)", s[1], s[2], 100*s[1]/s[2]), sig))
+  end
 
-function banditEstimation(b)  # This works, but 'wins' is not correct!
-  B = [button("Arm $i") for i = 1:numArms(b)]
-  tries = [foldp((acc, value) -> acc + 1, 0, signal(B[i])) for i = 1:arms]
-  wins = [foldp((acc, value) -> acc + pull(b,i), 0, signal(B[i])) for i = 1:arms]
-  for i = 1:numArms(b)
-    display(B[i])
-    display(map((w,t) -> latex(@sprintf("%d wins out of %d tries (%d percent)", w, t, 100*w/t)), wins[i], tries[i]))
-  end
-  display(map((w1,t1,w2,t2)->
-       Axis([
-              Plots.Linear(θ->pdf(Beta(w1+1, t1-w1+1), θ), (0,1), legendentry="Beta($(w1+1), $(t1-w1+1))"),
-              Plots.Linear(θ->pdf(Beta(w2+1, t2-w2+1), θ), (0,1), legendentry="Beta($(w2+1), $(t2-w2+1))")
-              ],
-            xmin=0,xmax=1,ymin=0),
-       wins[1], tries[1], wins[2], tries[2]
-       ))
-  t = togglebuttons(["Hide", "Show"], value="Hide", label="True parameters")
+  display(map((sig1,sig2)-> begin
+      w1, t1 = sig1[1], sig1[2]
+      w2, t2 = sig2[1], sig2[2]
+         Axis([
+                Plots.Linear(θ->pdf(Beta(w1+1, t1-w1+1), θ), (0,1), legendentry="Beta($(w1+1), $(t1-w1+1))"),
+                Plots.Linear(θ->pdf(Beta(w2+1, t2-w2+1), θ), (0,1), legendentry="Beta($(w2+1), $(t2-w2+1))")
+                ],
+      xmin=0,xmax=1,ymin=0, width="15cm", height="10cm")
+      end, sigs[1], sigs[2],
+         ))
+  t = togglebuttons(["Hide", "Show"], value="Hide", label="True Params")
   display(t)
-  display(map(v -> v == "Show" ? latex(string(b.θ)) : latex(""), signal(t)))
-end
-
-function banditEstimation_old(b)  # This doesn't work: 'tries' never gets updated!
-  B = [button("Arm $i") for i = 1:numArms(b)]
-  wins = [foldp((acc, value) -> acc + pull(b,i), 0, signal(B[i])) for i = 1:arms]
-  tries = [foldp((acc, value) -> acc + 1, 0, signal(B[i])) for i = 1:arms]
-  for i = 1:numArms(b)
-    display(B[i])
-    display(map((w,t) -> latex(@sprintf("%d wins out of %d tries (%d percent)", w, t, 100*w/t)), wins[i], tries[i]))
-  end
-  display(map((w1,t1,w2,t2)->
-       Axis([
-              Plots.Linear(θ->pdf(Beta(w1+1, t1-w1+1), θ), (0,1), legendentry="Beta($(w1+1), $(t1-w1+1))"),
-              Plots.Linear(θ->pdf(Beta(w2+1, t2-w2+1), θ), (0,1), legendentry="Beta($(w2+1), $(t2-w2+1))")
-              ],
-            xmin=0,xmax=1,ymin=0),
-       wins[1], tries[1], wins[2], tries[2]
-       ))
-  t = togglebuttons(["Hide", "Show"], value="Hide", label="True parameters")
-  display(t)
-  display(map(v -> v == "Show" ? latex(string(b.θ)) : latex(""), t))
+  display(map(v -> v == "Show" ? string(b.θ) : "", signal(t)))
 end
 
 type BanditStatistics
