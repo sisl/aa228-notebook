9ebd678423229d08ff36c3e599b516938f6bac97
Zachary Sunberg <sunbergzach@gmail.com>
2016-09-21 17:43:23 -0700

    I think the normal versions of the packages are sufficient

--- install.jl
+++ install.jl
@@ -1,11 +1,12 @@
 Pkg.add("NBInclude")
 Pkg.add("BayesNets")
-Pkg.checkout("BayesNets") # get the latest version
+# Pkg.checkout("BayesNets") # get the latest version
 Pkg.add("PGFPlots")
-Pkg.checkout("PGFPlots") # get the latest version
+# Pkg.checkout("PGFPlots") # get the latest version
 Pkg.add("Interact")
-Pkg.checkout("Interact") # get the latest version
+# Pkg.checkout("Interact") # get the latest version
 Pkg.add("RDatasets")
 Pkg.add("Grid")
 Pkg.add("Reactive")
 
+println("Dependency install complete!")
