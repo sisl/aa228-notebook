b720c1a2011765447a3ecf64b26cb239a4551411
Zachary Sunberg <sunbergzach@gmail.com>
2018-09-24 17:42:26 -0700

    fixed install.jl

--- install.jl
+++ install.jl
@@ -1,44 +1,24 @@
-Pkg.add("NBInclude")
-Pkg.add("BayesNets")
-Pkg.add("PGFPlots")
-Pkg.add("Interact")
-Pkg.add("RDatasets")
-Pkg.add("Reactive")
-Pkg.add("Plots")
-Pkg.add("PyCall")
-Pkg.add("PyPlot")
+import Pkg
 
-# POMDP packages
+@info("Adding JuliaPOMDP Package Registry to your global list of registries.")
 Pkg.add("POMDPs")
-Pkg.add("POMDPModelTools")
-Pkg.add("POMDPSimulators")
-Pkg.add("POMDPPolicies")
-Pkg.add("POMDPModels") # for Crying Baby
-
-try
-    Pkg.clone("https://github.com/zsunberg/ContinuumWorld.jl.git")
-catch ex
-    warn("The following error was encountered when cloning ContinuumWorld:")
-    showerror(STDERR, ex)
-    println()
-    warn("This error was ignored")
-end
-
 using POMDPs
-POMDPs.add("BasicPOMCP")
-POMDPs.add("MCTS")
-POMDPs.add("DiscreteValueIteration")
+POMDPs.add_registry()
 
+ENV["PYTHON"]=""
 
-# Needed for notebook 16
-try
-    Pkg.clone("https://github.com/zsunberg/LaserTag.jl")
-catch ex
-    warn("The following error was encountered when cloning LaserTag:")
-    showerror(STDERR, ex)
-    println()
-    warn("This error was ignored")
+projdir = dirname(@__FILE__())
+toml = open(joinpath(projdir, "Project.toml")) do f
+    Pkg.TOML.parse(f)
 end
+pkgs = collect(keys(toml["deps"]))
+pkgstring = string(pkg*"\n" for pkg in pkgs)
+@info("""
+    Installing the following packages to the current environment:
 
+    $pkgstring
+""")
 
-println("Dependency install complete! (check for errors)")
+Pkg.add(pkgs)
+
+@info("Dependency install complete! (check for errors)")
