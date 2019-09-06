a33ec9d5d8d97e42d2f56801ec849e7e3f8b002a
Robert Moss <rmoss92@gmail.com>
2019-09-05 23:34:58 -0700

    Confirmed fixes for runtests.jl and travis CI

--- runtests.jl
+++ runtests.jl
@@ -6,7 +6,7 @@ using Test
         # if endswith(d, ".ipynb") && !startswith(d, "08-Markov") && !startswith(d, "09-") && !startswith(d, "11-") && !startswith(d, "16-") && !startswith(d, "POM") # ignore MDP notebook because it fails for some reason
         if endswith(d, ".ipynb")
             @info("Running "*d)
-            stuff = "using NBInclude; @nbinclude(\"" * d * "\")"
+            stuff = "using InteractiveUtils; using NBInclude; ENV[\"PYTHON\"]=\"\"; @nbinclude(\"" * d * "\")"
             projdir = dirname(@__FILE__())
             cmd = `julia --project=$projdir -e $stuff`
             proc = run(pipeline(cmd, stderr=stderr), wait=false)
